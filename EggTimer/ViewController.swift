15c854df4a585350bec9246321e37390d508edd4
dbunk903 <dbunk903@gmail.com>
2022-02-24 15:58:03 +0900

    Egg Timer Counter

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,7 +6,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //let softTime = 5
+    //let mediumTime = 7
+    //let hardTime = 12
+    let eggTimes = ["Soft": 5,"Medium":7, "Hard":12]
+    var counter = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+
+        let hardness = sender.currentTitle!
+        
+        counter = eggTimes[hardness]! * 60
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
+    @objc func update(){
+        if counter > 0 {
+            print("\(String(describing:counter)) until your eggs are cooked")
+            counter-=1
+        }
+    }
+        
     
 
-
+    
+    
 }
