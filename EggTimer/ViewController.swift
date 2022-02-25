fabb6f27332269a8c1bc83d0dc12d939e9a229ee
dbunk903 <dbunk903@gmail.com>
2022-02-25 10:17:06 +0900

    Modified Progress Bar - Complete

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,32 +6,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //let softTime = 5
-    //let mediumTime = 7
-    //let hardTime = 12
+    
     let eggTimes = ["Soft": 5,"Medium":7, "Hard":12]
     var counter = 0
     var timer = Timer()
+    var secondsPassed = 0
     @IBOutlet weak var frontMessages: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-            
         timer.invalidate()
-        
         let hardness = sender.currentTitle!
-        
         counter = eggTimes[hardness]! // * 60
-        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        frontMessages.text = hardness
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
-        // we never stopped it.
-        
-        
     }
+    
     @objc func update(){
-        if counter > 0 {
-            print("\(String(describing:counter)) until your eggs are cooked")
-            counter-=1
-        }
-        else {
+        if secondsPassed < counter {
+            
+            secondsPassed += 1
+            
+            progressBar.progress = Float(secondsPassed) / Float(counter)
+            
+
+        } else {
             timer.invalidate()
             frontMessages.text = "DONE!!!"
         }
