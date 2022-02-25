ca8b6de8fbf919d01c814ca0b94cf89b5eb888b8
dbunk903 <dbunk903@gmail.com>
2022-02-25 10:34:24 +0900

    Challenge : added Alarm Audio when timer goes off

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -4,17 +4,18 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 5,"Medium":7, "Hard":12]
     var counter = 0
     var timer = Timer()
     var secondsPassed = 0
+    var player: AVAudioPlayer!
     @IBOutlet weak var frontMessages: UILabel!
     
     @IBOutlet weak var progressBar: UIProgressView!
-    
+        
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
         let hardness = sender.currentTitle!
@@ -35,8 +36,15 @@ class ViewController: UIViewController {
 
         } else {
             timer.invalidate()
+            playSound(soundName: "alarm_sound")
             frontMessages.text = "DONE!!!"
         }
+    }
+    
+    func playSound(soundName: String){
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 }
 
